94397fd5c5381f5d0a0f0266068328543912944e
ElLuchoMan <luchoman96@gmail.com>
2021-02-07 18:15:27 -0500

    Listado Responsive

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -5,8 +5,7 @@ import { Heroe } from '../../interfaces/hereoes.interface';
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styles: [
-  ]
+  styles: [`mat-card{ margin-top:20px}`]
 })
 export class ListadoComponent implements OnInit {
 heroes:Heroe[]=[]
