425228c87ed6ddc520f31e32152dfd2f5166a12f
ElLuchoMan <luchoman96@gmail.com>
2021-02-07 14:30:59 -0500

    Agregada ventana principal

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { HeroesRoutingModule } from './heroes-routing.module';
+import { MaterialModule } from '../material/material.module';
+
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
-import { HeroesRoutingModule } from './heroes-routing.module';
+
+
 
 
 
@@ -18,7 +24,9 @@ import { HeroesRoutingModule } from './heroes-routing.module';
     ListadoComponent],
   imports: [
     CommonModule,
+    FlexLayoutModule,
     HeroesRoutingModule,
+    MaterialModule,
   ]
 })
 export class HeroesModule { }
