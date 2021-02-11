27e543bb6fc5ec36d28f5357a93d087756b68b68
ElLuchoMan <luchoman96@gmail.com>
2021-02-10 20:45:29 -0500

    Agregada la opción de buscar

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { Heroe } from '../../interfaces/hereoes.interface';
 import { HeroesService } from '../../services/heroes.service';
-import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-buscar',
@@ -10,20 +10,36 @@ import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
   ]
 })
 export class BuscarComponent implements OnInit {
-  termino: string = "";
+
+  termino: string  = '';
   heroes: Heroe[] = [];
-  heroeSeleccionado!: Heroe;
-  constructor(private heroesService: HeroesService) { }
+  heroeSeleccionado: Heroe | undefined;
+
+  constructor( private heroesService: HeroesService ) { }
 
   ngOnInit(): void {
   }
+
+
   buscando() {
-    this.heroesService.getSugerencias(this.termino).subscribe(heroes => this.heroes = heroes);
-  }
-  opcionSeleccionada(event: MatAutocompleteSelectedEvent) {
-    const heroe: Heroe = event.option.value;
-    this.termino = heroe.superhero;
-    this.heroesService.getHeroePorId(heroe.id!).subscribe(heroe => this.heroeSeleccionado = heroe);
+
+    this.heroesService.getSugerencias( this.termino.trim() )
+      .subscribe( heroes => this.heroes = heroes );
 
   }
+
+  opcionSeleccionada( event: MatAutocompleteSelectedEvent ) {
+
+    if(!event.option.value) {
+      this.heroeSeleccionado = undefined;
+      return;
+    }
+
+    const heroe: Heroe = event.option.value;
+    this.termino = heroe.superhero;
+
+    this.heroesService.getHeroePorId( heroe.id! )
+      .subscribe( heroe => this.heroeSeleccionado = heroe );
+  }
+
 }
